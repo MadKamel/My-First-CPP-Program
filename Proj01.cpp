6febe9ca54732182d680b10c966c2226036efec5
Eric Rustrum <rustrumek@gmail.com>
2023-05-09 19:40:56 -0700

    Add project files.

---
+++ Proj01.cpp
@@ -0,0 +1,16 @@
+﻿#include "Proj01.h"
+
+int main()
+{
+    char x;
+    int i = 0;
+    std::cout << "Hello! Please type the letter c, three times.\n";
+    while (i < 3) {
+        std::cout << "> ";
+        std::cin >> x;
+        if (x == 'c') {
+            i += 1;
+        }
+    }
+    std::cout << "\nYou have typed three c's. Goodbye!";
+}
